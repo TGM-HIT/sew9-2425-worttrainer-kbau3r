b0ea3b80e8f7031924a718a27bff5a87de924a18
kbau3r <kbauer@student.tgm.ac.at>
2024-09-26 09:03:37 +0200

    JUnit tests functional

--- src/main/java/Test.java
+++
@@ -1,5 +0,0 @@
-/**
- * Tests
- */
-public class Test {
-}
---
+++ src/test/java/Test.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+
+    @Test
+    void testAddition() {
+        assertEquals(2, 1 + 1, "1 + 1 should equal 2");
+    }
+}
