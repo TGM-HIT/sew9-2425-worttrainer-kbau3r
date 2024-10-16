f936213ed6bafef46a967ab5bcf5f8d420307a4b
Kevin Bauer <kbauer@student.tgm.ac.at>
2024-10-16 12:04:49 +0200

    View code finished

--- src/main/java/View/View.java
+++ src/main/java/View/View.java
@@ -1,5 +1,30 @@
 package View;
 
+import Model.*;
+import javax.swing.JOptionPane;
+
 public class View {
 
+    public void showData(Data data) {
+        String message = "Wort: " + data.getName() + "\nURL: " + data.getUrl().toString();
+        JOptionPane.showMessageDialog(null, message, "Aktuelles Wort", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public String getInput() {
+        return JOptionPane.showInputDialog("Bitte das Wort eingeben:");
+    }
+
+    public void displayStatistics(Statistics statistics) {
+        String message = "Richtige Versuche: " + statistics.getCorrectAttempts() +
+                "\nFalsche Versuche: " + statistics.getWrongAttempts();
+        JOptionPane.showMessageDialog(null, message, "Statistik", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public void showMessage(String message) {
+        JOptionPane.showMessageDialog(null, message, "Information", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public void displayError(String error) {
+        JOptionPane.showMessageDialog(null, error, "Fehler", JOptionPane.ERROR_MESSAGE);
+    }
 }
